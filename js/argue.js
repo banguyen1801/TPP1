776b01d61876a74068f3a1f2e6d8ed85049c6545
Ba Nguyen <b.pynguyen@gmail.com>
2019-06-02 23:06:58 -0400

    homework3 completed

---
+++ js/argue.js
@@ -0,0 +1,6 @@
+document.getElementById("btn-us").addEventListener("click", function(){
+    document.getElementById("arguement").innerText="I'm right";
+});
+document.getElementById("btn-they").addEventListener("click", function(){
+    document.getElementById("arguement").innerText="No,I'm right!";
+});
---
+++ js/hover.js
@@ -0,0 +1,3 @@
+document.getElementById("dontHover").addEventListener("mouseover", function(){
+    alert("Hey, I told you to not hover over me!");
+});
